cfeefa173503dc4ef98232f3357da6026d8b9a0f
NA\ccarlson <doughtnerd@gmail.com>
2017-02-26 00:20:56 -0700

    Initial

---
+++ ImageSorter/ImageSorter/Model.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetadataExtractor;
+using System.IO;
+
+namespace ImageSorter
+{
+    public class Model
+    {
+        /// <summary>
+        /// Dictionary indicating which files belong to which date.
+        /// </summary>
+        private Dictionary<DateTime, string> dateMap;
+
+        /// <summary>
+        /// Dictionary where the source image path is the key, the target sorted path is the value.
+        /// </summary>
+        private Dictionary<string, string> pathMap;
+
+        /// <summary>
+        /// Dictionary indicating renaming of any duplicate file names that were found.
+        /// </summary>
+        private Dictionary<string, string> duplicateMap;
+
+        /// <summary>
+        /// List of images that could not be sorted.
+        /// </summary>
+        private List<string> unableToSort;
+
+        private ImageSortOptions sortOptions = 0x0000;
+
+        public ImageSortOptions SortOptions
+        {
+            get
+            {
+                return sortOptions;
+            }
+            set
+            {
+                sortOptions = value;
+            }
+        }
+
+        public Model()
+        {
+            dateMap = new Dictionary<DateTime, string>();
+            pathMap = new Dictionary<string, string>();
+            duplicateMap = new Dictionary<string, string>();
+            unableToSort = new List<string>();
+        }
+
+        /// <summary>
+        /// Handles sorting the image files in the root folder and producing the sorted results in the target.
+        /// </summary>
+        public void SortFolder(string rootPath, string targetPath)
+        {
+            DirectoryInfo rootInfo = System.IO.Directory.CreateDirectory(rootPath);
+            DirectoryInfo targetInfo = System.IO.Directory.CreateDirectory(targetPath);
+        }
+
+
+        public IEnumerable<Tag> ExtractExIfDateTags(MetadataExtractor.Directory dir)
+        {
+            List<Tag> tags = new List<Tag>();
+            foreach(Tag t in dir.Tags)
+            {
+                if (t.Name.ToLower().Contains("date")){
+                    tags.Add(t);
+                }
+            }
+            return tags;
+        }
+
+        public IEnumerable<MetadataExtractor.Directory> ExtractExIfDirectories(string imagePath)
+        {
+            List<MetadataExtractor.Directory> list = new List<MetadataExtractor.Directory>();
+            foreach(MetadataExtractor.Directory dir in ImageMetadataReader.ReadMetadata(imagePath))
+            {
+                if (dir.Name.ToLower().Contains("exif"))
+                {
+                    list.Add(dir);
+                }
+            }
+            return list;
+        }
+
+        private IEnumerable<MetadataExtractor.Directory> ExtractMetaDataDirectories(string imagePath)
+        {
+            return ImageMetadataReader.ReadMetadata(imagePath);
+        }
+
+        public struct ImageSortOptions
+        {
+            private byte value;
+
+            public byte Value {
+                get
+                {
+                    return this.value;
+                }
+                private set
+                {
+                    this.value = value;
+                }
+            }
+
+            public ImageSortOptions(byte value)
+            {
+                this.value = value;
+            }
+
+            public static implicit operator ImageSortOptions(byte value)
+            {
+                return new ImageSortOptions(value);
+            }
+
+            public static implicit operator byte(ImageSortOptions options)
+            {
+                return options.Value;
+            }
+
+            public static bool operator ==(ImageSortOptions first, ImageSortOptions second)
+            {
+                return first.Value == second.Value;
+            }
+
+            public static bool operator !=(ImageSortOptions first, ImageSortOptions second)
+            {
+                return first.Value != second.Value;
+            }
+
+            /// <summary>
+            /// Enum representing the possible options this class is capable of handling.
+            /// </summary>
+            public enum Options : byte
+            {
+                COPY_FILES = 0x0001,
+                ALLOW_DUPLICATES = 0x0010
+            }
+        }
+    }
+}
---
+++ ImageSorter/ImageSorterTests/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImageSorter;
+using System.Collections.Generic;
+
+namespace ImageSorterTests
+{
+    #region Program Model Tests
+
+    [TestClass]
+    public class ModelTests
+    {
+        [TestMethod]
+        public void TestImageSortOptions()
+        {
+            Model model = new Model();
+            Assert.AreEqual(new Model.ImageSortOptions(0) , model.SortOptions);
+
+            model.SortOptions = 0x1;
+            Assert.AreEqual(new Model.ImageSortOptions(1), model.SortOptions);
+
+            model.SortOptions = 0x10;
+            Assert.AreEqual(new Model.ImageSortOptions(16), model.SortOptions);
+
+            model.SortOptions = 0x11;
+            Assert.AreEqual(new Model.ImageSortOptions(17), model.SortOptions);
+
+            model.SortOptions = 0x1 | 0x10;
+            Assert.AreEqual(new Model.ImageSortOptions(17), model.SortOptions);
+
+            model.SortOptions = 0x1 & 0x11;
+            Assert.AreEqual(new Model.ImageSortOptions(1), model.SortOptions);
+
+            model.SortOptions = 0x1 & 0x12;
+            Assert.AreEqual(new Model.ImageSortOptions(0), model.SortOptions);
+        }
+
+        [TestMethod]
+        public void TestExtractImageExIfs()
+        {
+            Model model = new Model();
+            List<MetadataExtractor.Directory> list = new List<MetadataExtractor.Directory>(model.ExtractExIfDirectories("../../Apple iPhone 4.jpg"));
+            Assert.AreEqual("Exif IFD0", list[0].Name);
+            Assert.AreEqual("Exif SubIFD", list[1].Name);
+        }
+
+    }
+
+    #endregion
+}
